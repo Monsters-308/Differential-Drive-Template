6a1e25beac046d571b399adecf962dbdafdc8ad1
tg1523342 <tg1523342@icloud.com>
2025-09-20 11:08:36 -0400

    Initial commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,121 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.pathplanner.lib.config.ModuleConfig;
+import com.pathplanner.lib.config.RobotConfig;
+import com.pathplanner.lib.controllers.PPLTVController;
+import com.pathplanner.lib.controllers.PathFollowingController;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.util.Units;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+    private Constants() {
+        throw new UnsupportedOperationException("This is a constants class!");
+    }
+
+    public static class OperatorConstants {
+        private OperatorConstants() {
+            throw new UnsupportedOperationException("This is a constants class!");
+        }
+
+        // ports for the controllers
+        public static final int kDriverControllerPort = 0;
+        public static final int kCoDriverControllerPort = 1;
+    }
+
+    public static class DriveConstants {
+        private DriveConstants() {
+            throw new UnsupportedOperationException("This is a constants class!");
+        }
+
+        // ids for the motors
+        public static final int kFrontLeftMotorId = 3;
+        public static final int kFrontRightMotorId = 5;
+        public static final int kBackLeftMotorId = 2;
+        public static final int kBackRightMotorId = 4;
+
+        // sets if an motor is inverted
+        public static final boolean kLeftMotorsInverted = false;
+        public static final boolean kRightMotorsInverted = true;
+
+        // idle mode
+        public static final IdleMode kMotorIdleMode = IdleMode.kBrake;
+
+        // smartLimit
+        public static final int kSmartCurrentLimit = 30;
+
+        public static final double kMaxSpeedMetersPerSecond = 5.6;
+
+        // PID constants for controlling wheel velocity
+        public static final double kVelocityP = 0.1;
+        public static final double kVelocityI = 0.0;
+        public static final double kVelocityD = 0.0;
+        public static final double kVelocityFF = 1 / 473;
+
+        // physical constants
+        public static final double kWheelRadius = Units.inchesToMeters(3);
+        public static final double kWheelDiameter = 2 * kWheelRadius;
+        public static final double kWheelCircumference = kWheelDiameter * Math.PI;
+        public static final double kGearRatio = 8.45865;
+
+        public static final double kTrackWidth = 0.5;
+
+        // encoder conversion factors
+        public static final double kRotationsToMeters = kWheelCircumference / kGearRatio;
+        public static final double KRotationsPerMinuteToMetersPerSecond = kRotationsToMeters / 60;
+
+        // kinematics
+        public static final DifferentialDriveKinematics kDriveKinematics = new DifferentialDriveKinematics(kTrackWidth);
+
+        // this makes it so if there any stick drift it prevents the robot from move due
+        // to the stick drift
+        public static final double kDeadBand = 0.01;
+
+        public static final double kForwardAxisSensitvity = 0.4;
+        public static final double kRotatonAxisSenitvity = 0.4;
+
+        public static final double kForwardAxisMultiplier = 1;
+        public static final double kTurningAxisMultiplier = 0.7;
+    }
+
+    public static class AutoConstants {
+        private AutoConstants() {
+            throw new UnsupportedOperationException("This is a constants class!");
+        }
+
+        public static final double kMassKG = 50;
+        public static final double kRobotMOI = 50;
+
+        public static final double kWheelCOF = 1;
+
+        public static final DCMotor kDriveMotor = DCMotor.getNEO(2);
+
+        public static final double kDescretizationTime = 0.01;
+
+        public static final RobotConfig kRobotConfig = new RobotConfig(kMassKG, kRobotMOI,
+                new ModuleConfig(DriveConstants.kWheelDiameter / 2, DriveConstants.kMaxSpeedMetersPerSecond,
+                        kWheelCOF, kDriveMotor, DriveConstants.kSmartCurrentLimit, 2),
+                DriveConstants.kTrackWidth);
+
+        public static final PathFollowingController kAutoController = new PPLTVController(kDescretizationTime,
+                DriveConstants.kMaxSpeedMetersPerSecond);
+    }
+}
---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,81 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.subsystems.DriveTrain;
+import frc.robot.utils.CommandFlightHotasX;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
+ * subsystems, commands, and trigger mappings) should be declared here.
+ */
+public class RobotContainer {
+
+    // The robot's subsystem defined here
+    private final DriveTrain m_driveTrain = new DriveTrain();
+
+    // controllers
+    private final CommandFlightHotasX m_driverController = new CommandFlightHotasX(OperatorConstants.kDriverControllerPort);
+    private final CommandXboxController m_coDriverController = new CommandXboxController(OperatorConstants.kCoDriverControllerPort);
+
+    /**
+     * The container for the robot. Contains subsystems, OI devices, and commands.
+     */
+    public RobotContainer() {
+        configureBindings();
+    }
+
+    /**
+     * Use this method to define your trigger->command mappings. Triggers can be
+     * created via the
+     * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with
+     * an arbitrary
+     * predicate, or via the named factories in {@link
+     * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
+     * {@link
+     * CommandXboxController
+     * Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+     * PS4} controllers or
+     * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+     * joysticks}.
+     */
+    private void configureBindings() {
+        m_driveTrain.setDefaultCommand(
+                m_driveTrain.driveJoysticks(m_driverController::getThrottle, m_driverController::getStickX,
+                        m_driverController.getHID()::getL1Button));
+
+        ShuffleboardTab operatorTab = Shuffleboard.getTab("Operator");
+
+        operatorTab.addBoolean("L1", m_driverController.getHID()::getL1Button);
+        operatorTab.addBoolean("L2", m_driverController.getHID()::getL2Button);
+        operatorTab.addBoolean("L3", m_driverController.getHID()::getL3Button);
+        operatorTab.addBoolean("R1", m_driverController.getHID()::getR1Button);
+        operatorTab.addBoolean("R2", m_driverController.getHID()::getR2Button);
+        operatorTab.addBoolean("R3", m_driverController.getHID()::getR3Button);
+        operatorTab.addBoolean("5", m_driverController.getHID()::get5Button);
+        operatorTab.addBoolean("6", m_driverController.getHID()::get6Button);
+        operatorTab.addBoolean("7", m_driverController.getHID()::get7Button);
+        operatorTab.addBoolean("8", m_driverController.getHID()::get8Button);
+        operatorTab.addBoolean("Start", m_driverController.getHID()::getStartButton);
+        operatorTab.addBoolean("Select", m_driverController.getHID()::getSelectButton);
+
+        operatorTab.addDouble("Stick X", m_driverController::getStickX);
+        operatorTab.addDouble("Stick Y", m_driverController::getStickY);
+        operatorTab.addDouble("Throttle", m_driverController::getThrottle);
+        operatorTab.addDouble("Rudder", m_driverController::getRudder);
+        operatorTab.addDouble("Rocker", m_driverController::getRockerAxis);
+        operatorTab.addDouble("POV", m_driverController.getHID()::getPOV);
+    }
+}
