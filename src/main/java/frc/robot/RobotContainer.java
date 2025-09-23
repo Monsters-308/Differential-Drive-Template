16e86c917fa0b43236778a0babc9da7f3fd73ff7
tg1523342 <tg1523342@icloud.com>
2025-09-23 12:53:50 -0400

    Removed extra space

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -27,7 +27,6 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  * commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
-
     // The robot's subsystem defined here
     private final DriveTrain m_driveTrain = new DriveTrain();
 
